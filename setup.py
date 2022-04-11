0aeff233a907f8cbe15006b715e4c3a799556c28
Julien <julien.aldon@epitech.eu>
2022-04-11 09:31:30 +0200

    Add new version builder

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ build_exe_options = {"packages": ['gi']}
 
 setup(
     name="EpitechEventPlanner",
-    version="0.0.1",
+    version="0.1.1",
     description="Créer des event sur une activitée donnée",
     options={"build_exe": build_exe_options},
     executables=[Executable("interface.py", target_name="EpitechEventPlanner")],
