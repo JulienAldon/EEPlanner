a7942ca3fc72e730c3dbe1e2ebccdf5eb1eeb46a
Julien <julien.aldon@epitech.eu>
2022-04-11 09:30:20 +0200

    Add refacto

---
+++ constants.py
@@ -0,0 +1,8 @@
+PROMOTIONS = {
+    'msc1': 'msc4',
+    'msc2': 'msc5',
+    'premsc': 'msc3',
+    'wac1': 'wac1',
+    'wac2': 'wac2'
+}
+ACTIVITY_URL = f'/module/2021/W-ADM-007/LYN-0-1/acti-505014/'
--- model.py
+++ model.py
@@ -1,138 +1,37 @@
-import requests
-from time import sleep
-import re
+from constants import ACTIVITY_URL
 
-INTRA_LINK = r'intra\.epitech\.eu'
-activity_url = f'/module/2021/W-ADM-007/LYN-0-1/acti-505014/'
+class EventPlanner():
+    def __init__(self, intranet):
+        self.intranet = intranet
 
-PROMOTIONS = {
-    'msc1': 'msc4',
-    'msc2': 'msc5',
-    'premsc': 'msc3',
-    'wac1': 'wac1',
-    'wac2': 'wac2'
-}
-PLANIFICATION_HOURS = [
-    '9:00:00',
-    '12:00:00',
-    '13:30:00',
-    '17:00:00'
-]
+    def students_registration(self, event, promotions):
+        """Register promotion
 
-def exec_regex(reg, tstr):
-    """return true if reg is found in tstr false otherwise
-    """
-    matches = re.finditer(reg, tstr, re.MULTILINE)
-    for match in matches:
-        if match:
-            return True
-    return False
-
-class Intra:
-    """Intra exchange facilities, a token must be set and valid before calling any other methods
-    """
-    def __init__(self, token="", planification_hours=PLANIFICATION_HOURS):
-        self.token = Intra.check_autologin(token)
-        self.planification_hours = planification_hours
-    
-    def set_token(self, token):
-        self.token = Intra.check_autologin(token)
-        return self.token
-
-    def set_planification_hours(self, hours):
-        self.planification_hours = hours
-
-    @staticmethod
-    def check_hour_format(hour):
-        if not exec_regex(r'^([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$', hour):
-            return False
-        return True
-
-    @staticmethod
-    def check_autologin(autologin):
-        if exec_regex(r'^http://', autologin):
-            autologin = autologin.replace('http', 'https')
-        if exec_regex(rf'^{INTRA_LINK}/auth-[0-9a-fA-F]+$', autologin):
-            autologin = 'https://' + autologin
-            return autologin
-        elif exec_regex(r'^auth-[0-9a-fA-F]+$', autologin):
-            return f'https://intra.epitech.eu/' + autologin
-        elif exec_regex(rf'^https://{INTRA_LINK}/auth-[0-9a-fA-F]+$', autologin):
-            return autologin
-        return False
-
-    def get_promotions(self, selected_promotions):
-        """fetch selected promotions student list
-        selected_promotions can be:
-            - msc1
-            - msc2
-            - wac1
-            - wac2
-            - premsc
+        :param event: Complete event URL
+        :type event: str
+        :param promotions: promotions to register ('described in Constants file')
+        :type promotions: list[str]
+        :returns: Registered students or false otherwise
+        :rtype: typing.Optional(list[str])
         """
-        promotions = []
-        nb_items = 0
-        total = 1
-        promo = '|'.join(list(map(PROMOTIONS.get, selected_promotions)))
-        while nb_items < total:
-            req = requests.get(self.token + f'/user/filter/user?format=json&location=FR/LYN&year=2021&active=true&promo={promo}&offset={nb_items}')
-            result = req.json()
-            promotions += [elem['login'] for elem in result['items']]
-            total = result['total']
-            nb_items += len(result['items'])
-        return promotions
-
-    def register_students(self, selected_promotions, event):
-        """register selected_students to each event given in parameter
-        """
-        if not self.token:
-            print(self.token)
-            print('Error : token is not valid')
+        if not promotions or len(promotions) == 0:
             return None
-        try:
-            students = self.get_promotions(selected_promotions)
-        except Exception as e:
-            print(f'An error occured when retreiving intranet promotions : {e}')
-            return None
+        students = self.intranet.getStudents(promotions)
         if not students:
             return None
-        data = {f'items[{index}][login]': row for index, row in enumerate(students)}
-        try:
-            req = requests.post(self.token+activity_url + event + '/updateregistered?format=json', data)
-        except Exception as e:
-            print(f'An error occured while asking intra to register students : {e}')
-            return None
-        sleep(0.2) # HACK: avoid intra rate limit
-        return (req.status_code, req.json())
+        return self.intranet.registerStudents(event, students)
+    
+    def planify_sessions(self, dates, hours):
+        """Create pce sign event given a list of dates and list of hours
 
-    def planify_sessions(self, dates):
-        """create events for each timestamps in the current schedule (dates & self.planification_hours)
+        :param dates: All dates to planify
+        :type dates: list[str]
+        :param hours: Hours to planify
+        :type hours: list[str]
+        :rtype: list[dict]
         """
-        print(self.planification_hours)
-        if not self.token:
-            print(self.token)
-            print("Error : token is not valid")
+        if not dates or not hours or len(dates) == 0 or len(hours) == 0:
             return None
-        for date in dates:
-            for hour in self.planification_hours:
-                acti_data = {
-                    'start': date + ' ' + hour,
-                }
-                try:
-                    req = requests.post(self.token+activity_url+'planify?format=json', acti_data)
-                    sleep(0.2) # HACK: avoid intra rate limit
-                except Exception as e:
-                    print(f'An error occured while creating activities : {e}')
-                    return None
-        try:
-            activities = requests.get(self.token+activity_url+'?format=json')
-            sleep(0.2) # HACK: avoid intra rate limit
-        except Exception as e:
-            print(f'An error occured while retrieving activities : {e}') 
-            return None
-        activities_json = activities.json()
-        created_activities = [i['code'] for i in activities_json['events'] if i['begin'][:10] in dates]
-        if len(created_activities) <= 0:
-            print("An error occured : No session created")
-            return None
-        return created_activities
+        res = [self.intranet.createEvent(ACTIVITY_URL, date, hour) for date in dates for hour in hours]
+        return self.intranet.getEvents(ACTIVITY_URL, dates[0])
+        
