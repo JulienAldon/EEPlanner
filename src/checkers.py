25730c9cf60a5bb0b88617abc4f48286f8f7c29a
Julien <julien.aldon@epitech.eu>
2022-04-11 17:15:59 +0200

    Rename checkers for camelcase

--- src/checkers.py
+++ src/checkers.py
@@ -2,7 +2,7 @@ import re
 
 INTRA_LINK = r'intra\.epitech\.eu'
 
-def exec_regex(regex, string):
+def execRegex(regex, string):
     """Check if regex match in string
 
     :params regex: A regex
@@ -25,14 +25,14 @@ def check_autologin(autologin):
     :returns: A correctly formated autologin url if the format is correct, False otherwise
     :rtype: str
     """
-    if exec_regex(r'^http://', autologin):
+    if execRegex(r'^http://', autologin):
         autologin = autologin.replace('http', 'https')
-    if exec_regex(rf'^{INTRA_LINK}/auth-[0-9a-fA-F]+$', autologin):
+    if execRegex(rf'^{INTRA_LINK}/auth-[0-9a-fA-F]+$', autologin):
         autologin = 'https://' + autologin
         return autologin
-    elif exec_regex(r'^auth-[0-9a-fA-F]+$', autologin):
+    elif execRegex(r'^auth-[0-9a-fA-F]+$', autologin):
         return f'https://intra.epitech.eu/' + autologin
-    elif exec_regex(rf'^https://{INTRA_LINK}/auth-[0-9a-fA-F]+$', autologin):
+    elif execRegex(rf'^https://{INTRA_LINK}/auth-[0-9a-fA-F]+$', autologin):
         return autologin
     return False
 
@@ -41,6 +41,6 @@ def check_hour_format(hour):
     :param hour: Hour format (HH-MM-SS)
     :returns: A boolean, True if the format is correct, False otherwise
     """
-    if not exec_regex(r'^([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$', hour):
+    if not execRegex(r'^([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$', hour):
         return False
     return True
